672d4af8043199e2e87a0374726f5c5deff1e6ac
changchang240402 <thuytrangdao240402@gmail.com>
2023-08-18 16:39:59 +0700

    changchang1

--- servicios/_conexion.php
+++ servicios/_conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $host = 'database-1.crrgeosgu4h6.ap-southeast-1.rds.amazonaws.com:3306'; // El nombre del servicio de la base de datos en el archivo docker-compose.yml; // El puerto mapeado en el archivo docker-compose.yml
+    $host = 'database.crrgeosgu4h6.ap-southeast-1.rds.amazonaws.com:3306'; // El nombre del servicio de la base de datos en el archivo docker-compose.yml; // El puerto mapeado en el archivo docker-compose.yml
     $database = 'sistema_ecommerce'; // El nombre de la base de datos definido en el archivo docker-compose.yml
     $user = 'admin'; // El usuario definido en el archivo docker-compose.yml
     $password = 'adminpassword123'; // La contraseña definida en el archivo docker-compose.yml
