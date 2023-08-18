ff0f5211e64ee985974268eea21ccb9a7434a80c
changchang240402 <thuytrangdao240402@gmail.com>
2023-08-18 16:21:56 +0700

    changchang

--- servicios/_conexion.php
+++ servicios/_conexion.php
@@ -1,8 +1,8 @@
 <?php
-    $host = '10.20.1.10:3306'; // El nombre del servicio de la base de datos en el archivo docker-compose.yml; // El puerto mapeado en el archivo docker-compose.yml
+    $host = 'database-1.crrgeosgu4h6.ap-southeast-1.rds.amazonaws.com:3306'; // El nombre del servicio de la base de datos en el archivo docker-compose.yml; // El puerto mapeado en el archivo docker-compose.yml
     $database = 'sistema_ecommerce'; // El nombre de la base de datos definido en el archivo docker-compose.yml
-    $user = 'hemish'; // El usuario definido en el archivo docker-compose.yml
-    $password = 'contrasena123'; // La contraseña definida en el archivo docker-compose.yml
+    $user = 'admin'; // El usuario definido en el archivo docker-compose.yml
+    $password = 'adminpassword123'; // La contraseña definida en el archivo docker-compose.yml
 
     $con = mysqli_connect($host, $user, $password, $database);
 ?>
